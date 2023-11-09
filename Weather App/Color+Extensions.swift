0047d5cb2345d05262a712c95528dc5b01d5596e
walex <walex.milto@abamais.com>
2023-11-09 18:17:12 -0300

    Adding status and hour forecast

---
+++ Weather App/Color+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by walex milto on 09/11/23.
+//
+
+import Foundation
---
+++ Weather App/HourlyForecastCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HourlyForecastCollectionViewCell.swift
+//  Weather App
+//
+//  Created by walex milto on 09/11/23.
+//
+
+import UIKit
+
+class HourlyForecastCollectionViewCell: UICollectionViewCell {
+    
+}
